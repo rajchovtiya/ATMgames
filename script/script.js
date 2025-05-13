53712b9824ff557a8c65853b45512a547bab31ae
rajchovatiya <rajchovatiya783@gmail.com>
2025-05-13 22:43:02 +0530

    add new code

--- script/script.js
+++ script/script.js
@@ -7,8 +7,8 @@ $(() => {
 
 
     $('.multiple-items').slick({
-        slidesToShow: 3.7,
-        slidesToScroll: 3,
+        slidesToShow: 3.5,
+        slidesToScroll: 3.5,
         autoplaySpeed: 2000,
         prevArrow: $('.leftarrow'),
         nextArrow: $('.rightarrow'),
@@ -22,6 +22,39 @@ $(() => {
             }
         ]
     });
+    let multiple_items = document.querySelectorAll(".multiple-items a img");
+    multiple_items.forEach((e) => {
+        e.addEventListener("click", (event) => {
+            let deta = event.target.getAttribute("secondlink");
+            let name = event.target.getAttribute("name");
+            localStorage.setItem("link", JSON.stringify(deta));
+            localStorage.setItem("link_name", JSON.stringify(name));
+
+        });
+    });
+
+    let hoverimg = document.querySelector(".hoverimg")
+    let hoverimgp = document.querySelector(".hoverimg p")
+
+    hoverimg.children[0].addEventListener("mousemove", () => {
+        hoverimgp.style.opacity = 1;
+    })
+    hoverimg.children[0].addEventListener("mouseleave", () => {
+        hoverimgp.style.opacity = 0;
+    })
+
 })
+
+$(() => {
+    let deat = JSON.parse(localStorage.getItem("link"));
+    let name = JSON.parse(localStorage.getItem("link_name"));
+
+    let game_iframe = document.querySelector("iframe");
+    let heding = document.querySelector(".heding")
+    if (deat && game_iframe) {
+        game_iframe.src = deat;
+    }
+    heding.innerText = name
+});
 
 
